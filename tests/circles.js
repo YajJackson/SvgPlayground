6d96b615743a395368d4bbe782e39d3120abec74
Jay Jackson <jaynickjackson@gmail.com>
2017-11-30 23:57:50 -0500

    started node creation, needs revision

--- tests/circles.js
+++ tests/circles.js
@@ -3,13 +3,7 @@ const paper = Snap()
 const max_x = window.innerWidth
 const max_y = window.innerHeight
 
-const createNode = (x, y, r) => {
-  c = paper.circle(x, y, r)
-  c
-    .attr({ class:'node draggable', id: c.id })
-    .drag(move, start, stop)
-}
-
+// move stuff
 const move = function(dx,dy) {
   this.attr
   ({
@@ -20,16 +14,30 @@ var start = function() {
   this.data('origTransform', this.transform().local )
 }
 var stop = function() {
-  // this.undrag()
+  this.undrag()
   console.log('finished dragging')
 }
 
+// create stuff
+const createNode = (x, y, r) => {
+  c = paper.circle(x, y, r)
+  c.attr({ class:'node draggable', id: c.id}).drag(move, start, stop)
+  $('.node').mouseup((e)=>{
+    node = e.target
+    radius = parseInt(node.getAttribute('r'))
+    matrix = node
+              .getAttribute('transform')
+              .split('').slice(0, -1).join('').split(',').slice(-2)
+              .map(i=> parseInt(i))
+
+    x_pos_end = parseInt(node.getAttribute('cx')) + matrix[0]
+    y_pos_end = parseInt(node.getAttribute('cy')) + matrix[1]
+
+    // console.log(`start: ${x_pos_start} , ${y_pos_start}`)
+    console.log(`start: ${x_pos_end} , ${y_pos_end}`)
+    createNode(x_pos_end, y_pos_end + radius*2, radius/1.5)
+  })
+}
+
+// test stuff
 createNode(max_x/2, max_y/2, 100)
-createNode(150, 150, 50)
-
-$('.node').click((e)=>{
-  node = e.target
-  x_pos = node.getAttribute('cx')
-  y_pos = node.getAttribute('cy')
-
-})
