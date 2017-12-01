6ac5cbee781b3e767cc1c9138f6bb71ec8d01fee
Jay Jackson <jaynickjackson@gmail.com>
2017-11-30 23:15:19 -0500

    draggable circles, yay!

--- tests/circles.js
+++ tests/circles.js
@@ -1,7 +1,35 @@
-var paper = Snap()
+const paper = Snap()
 
-max_x = window.innerWidth
-max_y = window.innerHeight
+const max_x = window.innerWidth
+const max_y = window.innerHeight
 
-var Circle = paper.circle(max_x/2, max_y/2, 100)
+const createNode = (x, y, r) => {
+  c = paper.circle(x, y, r)
+  c
+    .attr({ class:'node draggable', id: c.id })
+    .drag(move, start, stop)
+}
 
+const move = function(dx,dy) {
+  this.attr
+  ({
+      transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy]
+  });
+}
+var start = function() {
+  this.data('origTransform', this.transform().local )
+}
+var stop = function() {
+  // this.undrag()
+  console.log('finished dragging')
+}
+
+createNode(max_x/2, max_y/2, 100)
+createNode(150, 150, 50)
+
+$('.node').click((e)=>{
+  node = e.target
+  x_pos = node.getAttribute('cx')
+  y_pos = node.getAttribute('cy')
+
+})
